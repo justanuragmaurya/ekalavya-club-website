ff802258ecbef27fe202115e10475053ddfd54de
justanuragmaurya <justanuragmaurya@gmail.com>
2024-09-14 15:21:48 +0530

    admin page also done

--- app/api/findusers/route.tsx
+++ app/api/findusers/route.tsx
@@ -1,13 +1,18 @@
-
-import { NextResponse} from "next/server";
 import { PrismaClient } from '@prisma/client';
-
+import { cookies } from "next/headers";
 
 const client = new PrismaClient();
 
 export async function GET(){
-    
-    
+    const _ = cookies()
+    console.log(_)
     const data = await client.user.findMany();
-    return(NextResponse.json(data))
+    return new Response(JSON.stringify(data),{
+        headers:{
+            'Content-Type':'application/json',
+            'Cache-Control': 'no-store'
+        }
+    })
 }
+
+export const fetchCache = 'force-no-store'; 
